ffe056deb5a2ebaf1adf4eac7367ed8438862e25
M YAMINI <yaminishetty6@gmail.com>
2026-07-17 01:16:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,32 @@
+###############################################
+# Terraform Configuration
+###############################################
 
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+###############################################
+# AWS Provider Configuration
+###############################################
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Name        = "Terraform-IaC"
+      Project     = "AWS Infrastructure"
+      Environment = "Development"
+      Owner       = "Yamini Shetty"
+      ManagedBy   = "Terraform"
+    }
+  }
+}
