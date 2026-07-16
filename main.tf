ba801e6aa920a6e501f52cc9734232bc49dcf010
M YAMINI <yaminishetty6@gmail.com>
2026-07-17 01:17:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
+resource "aws_vpc" "main" {
 
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "Terraform-VPC"
+  }
+}
+
+resource "aws_subnet" "public" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.public_subnet_cidr
+
+  availability_zone = var.availability_zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
